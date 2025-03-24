59e186b8f30e8cb521cd8fe2f8fdadef4f082bbd
Pvdm1400 <vandermeulen@ogcleanfuels.com>
2025-03-24 23:56:39 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -349,8 +349,8 @@ if st.button("Genereer Route"):
 
     if not start or not end:
         st.error("Kon één van de adressen niet vinden.")
-        else:
-            waypoints, used_stations = build_route_with_filtered_tankstations(start, end, tankstations, interval_km=interval_km, corridor_km=corridor_km)
+else:
+    waypoints, used_stations = build_route_with_filtered_tankstations(start, end, tankstations, interval_km=interval_km, corridor_km=corridor_km)
         route_coords = get_osrm_route([(wp[0], wp[1]) for wp in waypoints])
         if route_coords:
             df = pd.DataFrame(route_coords, columns=["Longitude", "Latitude"])
